8f52b8812c9abdeb2535c03f0955f594298f46d7
ChemaLev <jose.levano1331@gmail.com>
2024-06-04 06:48:53 -0500

    feat: Initialize the proyect

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import './styles.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
